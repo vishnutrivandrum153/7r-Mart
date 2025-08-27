478679c8386978714055a792ca71c15471d5d24e
vishnu <vishnutrivandrum153@gmail.com>
2025-08-27 22:44:37 +0530

    Multi Browser testing using chrome and firefox

--- 7rMart/src/test/java/testScript/Base.java
+++ 7rMart/src/test/java/testScript/Base.java
@@ -5,9 +5,11 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 
 import utilities.ScreenShotUtility;
 import utilities.WaitUtility;
@@ -19,9 +21,21 @@ public class Base {
 	public WebDriver driver;
 	
 	@BeforeMethod(alwaysRun = true)// alwaysRun = true ensures that this method runs even if there are failures in the test methods
-	public void browserInitilization() 
+	@Parameters("browser")
+	public void browserInitilization(String browser) throws Exception 
+		{
+		if(browser.equalsIgnoreCase("chrome")) //equalIgnoreCaes is not case sensitive
 		{
 		driver = new ChromeDriver(); // Initialize the ChromeDriver
+		}
+	else if(browser.equalsIgnoreCase("firefox")) 
+		{
+		driver = new FirefoxDriver(); // Initialize the FirefoxDriver
+		}
+	else 
+		{
+		throw new Exception("Browser not supported");
+		}
 		driver.get("https://groceryapp.uniqassosiates.com/admin/");// method to open URL
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(WaitUtility.IMPLICITWAIT));
 		driver.manage().window().maximize(); //window maximization

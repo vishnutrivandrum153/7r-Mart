d471eac59b88ccb4b45dfb2f5e30056cda884370
vishnu <vishnutrivandrum153@gmail.com>
2025-08-27 19:47:12 +0530

    'Edit a news' tested

--- 7rMart/src/test/java/testScript/ManageNewsTest.java
+++ 7rMart/src/test/java/testScript/ManageNewsTest.java
@@ -12,11 +12,10 @@ import utilities.ExcelUtility;
 public class ManageNewsTest extends Base {
 	
 	@Test(priority = 1, groups= {"regression"})
-	public void postNews() throws IOException
+	public void postNewsTest() throws IOException
 	{
-		//String username = "admin";
-		//String password = "admin";
-		String username = ExcelUtility.getstringData(1,0, "LoginPage"); // Fetching username from Excel file. 1,0 represent cell position
+
+		String username = ExcelUtility.getstringData(1,0, "LoginPage"); 
 		String password = ExcelUtility.getstringData(1,1, "LoginPage");
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.enterUsername(username);
@@ -27,11 +26,31 @@ public class ManageNewsTest extends Base {
 		managenewspage.clickNewsDescription();
 		managenewspage.clickAddNewButton();
 		
-		String newsDescriptionText =  ExcelUtility.getstringData(0,0, "News"); // Fetching news description from Excel file. 0,0 represent cell position
+		String newsDescriptionText =  ExcelUtility.getstringData(0,0, "News"); 
 		managenewspage.enterNewsDescription(newsDescriptionText);
 		
 		managenewspage.clickSaveButton();
 		Assert.assertTrue(managenewspage.isAlertDisplayed());	
+	}
+	
+	@Test(priority = 2)
+	public void editNewsTest() throws IOException 
+	{
+	String username = ExcelUtility.getstringData(1,0, "LoginPage"); 
+	String password = ExcelUtility.getstringData(1,1, "LoginPage");
+	String updatedDescription = "This is an updated news description!!!.";
+	
+	LoginPage loginPage = new LoginPage(driver);
+	loginPage.enterUsername(username);
+	loginPage.enterPassword(password);
+	loginPage.sigin();
+	
+	ManageNewsPage managenewspage = new ManageNewsPage(driver);
+	managenewspage.clickNewsDescription();
+	managenewspage.clickEditNewsButton();
+	managenewspage.editNewsDescription(updatedDescription);
+	managenewspage.clickUpdateNewsButton();
+	Assert.assertTrue(managenewspage.isAlertDisplayed());
 	}
 
 }
